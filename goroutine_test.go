3bf6f033fa26a332cb45513aebe7ea4755af1a29
directoryx <anggawijaya4567@gmail.com>
2021-07-01 01:44:18 +0700

    channel as parameter

--- goroutine_test.go
+++ goroutine_test.go
@@ -44,3 +44,18 @@ func TestCreateChannel(t *testing.T)  {
 
 	time.Sleep(5* time.Second)
 }
+
+func TestChannelAsParameter(t *testing.T)  {
+	channel := make(chan string)
+
+	go GiveMeResponse(channel)
+
+	data := <- channel
+	fmt.Println(data)
+	close(channel)
+}
+
+func GiveMeResponse(channel chan string)  {
+	time.Sleep(2 * time.Second)
+	channel <- "Angga Wijaya"
+}
