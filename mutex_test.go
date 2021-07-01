b982fbe117667d7ef0b239b8d0bd5decaefc6ae6
directoryx <anggawijaya4567@gmail.com>
2021-07-01 13:59:27 +0700

    rwmutex

--- mutex_test.go
+++ mutex_test.go
@@ -29,3 +29,39 @@ func TestMutex(t *testing.T) {
 	fmt.Println("Counter = ",x)
 }
 
+type BankAccount struct {
+	// RWMutex untuk melakukan lock dan unlocking tetapi lebih fokus ke bagian read dan bagian write
+	// karena mutex biasa tidak mengakomodir 2 lock write dan read (lebih ribet)
+	RWMutex sync.RWMutex
+	Balance int
+}
+
+func (account *BankAccount) AddBalance(amount int)  {
+	account.RWMutex.Lock()
+	account.Balance = account.Balance + amount
+	account.RWMutex.Unlock()
+}
+
+func (account BankAccount) GetBalance() int {
+	account.RWMutex.RLock()
+	balance := account.Balance
+	account.RWMutex.RUnlock()
+	return balance
+}
+
+func TestRWMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total Balance",account.GetBalance())
+}
+
