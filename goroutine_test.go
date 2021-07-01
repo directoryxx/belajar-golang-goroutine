832086d15b15ee0904db2d966dbeadab3f24fa98
directoryx <anggawijaya4567@gmail.com>
2021-07-01 12:32:53 +0700

    Function parameter channel

--- goroutine_test.go
+++ goroutine_test.go
@@ -59,3 +59,23 @@ func GiveMeResponse(channel chan string)  {
 	time.Sleep(2 * time.Second)
 	channel <- "Angga Wijaya"
 }
+
+func OnlyIn(channel chan<- string)  {
+	time.Sleep(2 * time.Second)
+	channel <- "Angga"
+}
+
+func OnlyOut(channel <-chan string)  {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T)  {
+	channel := make(chan string)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(3 * time.Second)
+	close(channel)
+}
